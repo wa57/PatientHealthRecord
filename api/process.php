b5f6c19e3dbbab283155754ae46078f73c3767ca
Ubuntu <ubuntu@ip-172-31-18-253.ec2.internal>
2018-09-18 23:43:01 +0000

    [CRON] Auto Commit

--- api/process.php
+++ api/process.php
@@ -3,7 +3,7 @@ include 'db_connect.php';
 
 header('Content-Type: application/json');
 
-if(isset($_GET["GetAppointments"])) 
+if(isset($_GET["GetAllUnscheduledAppointments"])) 
 {
     $stmt = $db->query('SELECT * FROM appointments WHERE patient_id IS NULL');
     $json = $stmt->fetchAll(PDO::FETCH_ASSOC);
