2dedddb762c6088d678ac59765f40d229cb81dbd
Ubuntu <ubuntu@ip-172-31-18-253.ec2.internal>
2018-11-11 19:00:01 -0500

    [CRON] Auto Commit

---
+++ sql/InitializeData3.sql
@@ -0,0 +1,5 @@
+INSERT INTO appointments (appointment_id, physician_id, patient_id, date, time, appointment_status)
+VALUES
+	(NULL, "1", "5", "2018-10-16", "15:30:00", "1"),
+	(NULL, '3', '5', '2018-09-22', '13:15:00', '1'),
+	(NULL, '1', '5', '2018-10-19', '12:30:00', '1')
